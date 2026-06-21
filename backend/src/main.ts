c8dc8da1fa9cc863274f6521001c4cee9063517e
Sundas Khan <sundaskhanhere@gmail.com>
2026-06-21 23:02:44 +0500

    fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,7 +36,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(
     `🚀 Backend running at http://localhost:${process.env.PORT ?? 3000}/api`,
   );
